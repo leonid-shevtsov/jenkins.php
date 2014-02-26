ad6c2fe524f8256d956e687d5a4ad68d70549c73
Alexsandro Pereira <apssouza22@gmail.com>
2014-02-26 11:46:52 -0300

    removing comments unnecessary

--- JenkinsLogAnalyzer_CLI.php
+++ JenkinsLogAnalyzer_CLI.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of JenkinsLogAnalyzer_CLI
  *
@@ -16,7 +10,7 @@ class JenkinsLogAnalyzer_CLI {
 
     private $notifyAdapter;
 
-    function __construct($argc, $argv, $notifyAdapter)
+    function __construct($argc, $argv, INotifyAdapter $notifyAdapter)
     {
         $this->argc = $argc;
         $this->argv = $argv;
